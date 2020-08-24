2b0dcbef07284270cf56cf11132224c2d3f4a284
Rebekah <rebekah.kirk@srm.com>
2020-08-24 10:12:00 +0100

    Uploading final files

--- cyberpet/pet.js
+++ cyberpet/pet.js
@@ -12,8 +12,8 @@ class Animal{
     constructor(name, hunger, thirst, happiness){
         this._name = name;
         this._hunger = 50;
-        this._thirst = 100;
-        this._happiness = 100;
+        this._thirst = 50;
+        this._happiness = 50;
     }
     get name(){
         return this._name;
@@ -28,13 +28,13 @@ class Animal{
         return this._happiness;
     }
     eat(){
-        this._hunger++;
+        this._hunger--;
     }
     drink(){
-        this._thirst--;
+        this._thirst++;
     }
     happiness(){
-        this._happiness--;
+        this._happiness++;
     }
 }
 
@@ -66,17 +66,26 @@ dogPic.addEventListener("click", ()=>{
     catPic.style.visibility = "visible";
     fishPic.style.visibility = "hidden";
     catPic.src= "dog.png";
+    newDog = new Dog ("Buster");
 });
+
+// const getHungry = () =>{
+//     if(newCat._hunger>0){
+//         newCat.eat()
+//         hungerLevel.innerHTML = newCat._hunger
+//         setTimeout(getHungry, 2000)
+//     } else (newCat.hunger>100)
+// }
 
 catPic.addEventListener("click", ()=>{
     pet = "cat";
     dogPic.style.visibility = "hidden";
     fishPic.style.visibility = "hidden";
-
+    // getHungry();
     newCat = new Cat ("Meow");
-    console.log(newCat);
-    newCat.eat();
-    console.log(newCat);
+    // console.log(newCat);
+    // newCat.eat();
+    // console.log(newCat);
 });
 
 fishPic.addEventListener("click", ()=>{     
@@ -85,6 +94,7 @@ fishPic.addEventListener("click", ()=>{
     dogPic.style.visibility = "hidden";
     catPic.style.visibility = " visible";
     catPic.src = "fish.png";
+    newFish = new Fish ("Goldie")
 });
 
 let hungerNum = document.getElementById("hungerNum");
@@ -92,5 +102,30 @@ let thirstNum = document.getElementById("thirstNum");
 let happinessNum = document.getElementById("happinessNum");
 
 foodButton.addEventListener("click", () =>{
-    hungerLevel.textContent=newCat.hunger;
-})
+    hungerLevel.innerHTML = newCat._hunger++;
+});
+foodButton.addEventListener("click", () =>{
+    hungerLevel.innerHTML = newFish._hunger++;
+});
+foodButton.addEventListener("click", () =>{
+    hungerLevel.innerHTML = newDog._hunger++;
+});
+drinkButton.addEventListener("click", () =>{
+    thirstLevel.innerHTML = newCat._thirst++;
+});
+drinkButton.addEventListener("click", () =>{
+    thirstLevel.innerHTML = newFish._thirst++;
+});
+drinkButton.addEventListener("click", () =>{
+    thirstLevel.innerHTML = newDog._thirst++;
+});
+playButton.addEventListener("click", () =>{
+    happinessLevel.innerHTML = newCat._happiness++;
+});
+playButton.addEventListener("click", () =>{
+    happinessLevel.innerHTML = newFish._happiness++;
+});
+playButton.addEventListener("click", () =>{
+    happinessLevel.innerHTML = newDog._happiness++;
+});
+// foodButton.addEventListener("click", )
